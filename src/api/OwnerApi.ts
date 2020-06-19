753fe2f9d2c9953a2cb77c6a9d2363c6b6f8620b
Krzysztof Łopuch <klopuch@gmail.com>
2020-06-19 19:45:50 +0200

    back to axios

--- src/api/OwnerApi.ts
+++ src/api/OwnerApi.ts
@@ -1,19 +1,29 @@
-//import Axios, { AxiosInstance } from 'axios';
+import Axios, { AxiosInstance } from 'axios';
 import { SalesmanListItem } from './types';
-import { Cerber } from '../service/Cerber';
-
+// import { Cerber } from '../service/Cerber';
+const BaseUrl = `${process.env.REACT_APP_BASE_API}`;
 export class OwnerApi {
   private _token: string;
 
+  private api: AxiosInstance;
+
   constructor(token: string) {
     this._token = token;
+
+    this.api = Axios.create({
+      baseURL: `${BaseUrl}`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 
   public getList = async (): Promise<SalesmanListItem[]> => {
-    const result = await Cerber.getInstance().callProxy('/api/owner/salesman/list', '', 'get', this._token);
+    // const result = await Cerber.getInstance().callProxy('/api/owner/salesman/list', '', 'get', this._token);
+    const { data } = await this.api.get<SalesmanListItem[]>('/api/owner/salesman/list');
 
-    if (result && result.length > 0) {
-      return JSON.parse(result) as SalesmanListItem[];
+    if (data && data.length > 0) {
+      return data;
     }
 
     return [];
